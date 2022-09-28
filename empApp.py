94971343236eeef1dcef9106f79f30d59043c472
shiketsu <zhijie07147@gmail.com>
2022-09-28 16:00:43 +0800

    Update empApp.py

--- empApp.py
+++ empApp.py
@@ -65,13 +65,9 @@ def add():
             print("Data inserted in MySQL RDS... uploading image to S3...")
             # s3.Bucket(custombucket).put_object(Key=emp_image_file_name_in_s3, Body=emp_image_file)
             #s3.Bucket(custombucket).put_object(Key=emp_image_file_name_in_s3)
-            s3_bucket = current_app.config['S3_BUCKET']
-            s3_dir = current_app.config['S3_DIR']
-            response = s3.put_object(
-                Body = io.BufferedReader(emp_image_file).read(),
-                Bucket = custombucket,
-                Key = str(emp_id) + '.' + emp_image_file.filename.split('.')[1]
-            )
+            
+            s3.upload_file(io.BufferedReader(emp_image_file).read(), custombucket, str(emp_id) + '.' + emp_image_file.filename.split('.')[1])
+            
             bucket_location = boto3.client('s3').get_bucket_location(Bucket=custombucket)
             s3_location = (bucket_location['LocationConstraint']) 
             if s3_location is None:
