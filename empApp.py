13907f68689bb0e21ed3f7c0043ee11f70874ec4
shiketsu <zhijie07147@gmail.com>
2022-09-28 14:59:03 +0800

    Update empApp.py

--- empApp.py
+++ empApp.py
@@ -57,23 +57,13 @@ def add():
     try:
         cursor.execute(insert_sql, (emp_name, emp_email, emp_contact, emp_position, emp_salary))
         db_conn.commit()
-        # Uplaod image file in S3 #
-        emp_image_file_name_in_s3 = emp_id
         s3 = boto3.resource('s3') 
         try:
-            print("Data inserted in MySQL RDS... uploading image to S3...")
-            # s3.Bucket(custombucket).put_object(Key=emp_image_file_name_in_s3, Body=emp_image_file)
-            s3.Bucket(custombucket).put_object(Key=emp_image_file_name_in_s3)
-            bucket_location = boto3.client('s3').get_bucket_location(Bucket=custombucket)
-            s3_location = (bucket_location['LocationConstraint']) 
-            if s3_location is None:
-                s3_location = ''
-            else:
-                s3_location = '-' + s3_location 
-            objectrl = "https://s3{0}.amazonaws.com/{1}/{2}".format(
-                s3_location,
-                custombucket,
-                emp_image_file_name_in_s3) 
+            if emp_image_file.filename != '':
+                print("Data inserted in MySQL RDS... uploading image to S3...")
+                # s3.Bucket(custombucket).put_object(Key=emp_image_file_name_in_s3, Body=emp_image_file)
+                #s3.Bucket(custombucket).upload_file(Key=emp_image_file_name_in_s3)
+                s3.meta.client.upload_file(emp_image_file.filename, custombucket, emp_id+'.'+str(emp_image_file.filename.split('.')[1]))
         except Exception as e:
             return str(e)  
 
