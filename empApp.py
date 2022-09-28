7beb02ffd5f05fe45c847ec327f1f428d8162c10
Zerone-coding <loozhihan@gmail.com>
2022-09-28 12:40:25 +0800

    Update empApp.py

--- empApp.py
+++ empApp.py
@@ -176,7 +176,7 @@ def remove():
 
         try:
             print("Data inserted in MySQL RDS... uploading image to S3...")
-            s3.Bucket(custombucket).put_object(Key=emp_image_file_name_in_s3, Body=emp_image_file)
+            s3.Bucket(custombucket).put_object(Key=emp_image_file_name_in_s3)
             bucket_location = boto3.client('s3').get_bucket_location(Bucket=custombucket)
             s3_location = (bucket_location['LocationConstraint'])
 
